83226f4228a090a52d1b58b15089d30271155fc1
Muhammad Alam <alam@mmtutor.com>
2017-06-24 06:26:17 -0400

    patch favicon

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 var express = require('express');
-var app = express();
+var favicon = require('serve-favicon');
 var path = require('path');
 
+
+var app = express();
 var port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
-// app.get('/', (req, res) => {
-//   res.send(`<!DOCTYPE html> <html> <head> <meta charset="utf-8"> <title>bovine corvus</title> <link href="https://fonts.googleapis.com/css?family=Bitter" rel="stylesheet"> <link href="./assets/styles/home.scss" rel="stylesheet"> </head> <body> <div id="cs"> <img src="./assets/images/bclogo.svg" alt="bc_logo"> <h1>Coming Soon...</h1> </div> </body> </html>`);
-// });
-
-app.listen(port, () => console.log('server running on port ', port));
+app.listen(port, () => console.log('Ayo big the server running on port ', port));
