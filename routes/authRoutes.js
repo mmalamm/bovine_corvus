daf5793020f0a97560b76258e08128a7c8bf854a
Muhammad Alam <alam@mmtutor.com>
2017-08-07 01:41:11 -0400

    change authroutes.js back

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,13 +2,13 @@ const passport = require('passport');
 
 module.exports = app => {
   app.get(
-    'https://bovine-corvus.herokuapp.com/auth/github',
+    '/auth/github',
     passport.authenticate('github', {
       scope: ['user:email']
     })
   );
 
-  app.get('https://bovine-corvus.herokuapp.com/auth/github/callback', passport.authenticate('github', { failureRedirect: '/failed'}), (req, res) => res.send(req.user));
+  app.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/failed'}), (req, res) => res.send(req.user));
 
   app.get('/api/logout', (req, res) => {
     req.logout();
